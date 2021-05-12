89eb71a5e606cfbe95d867af1e41b33960dbdabd
hongyuanyang <hongyuanyang@huobi.com>
2021-05-12 11:28:55 +0800

    add truffle for deploy

--- contracts/contracts/Config.sol
+++ contracts/contracts/Config.sol
@@ -26,7 +26,7 @@ contract Config {
     uint8 constant SUCCESS_FLAG_BYTES = 1;
 
     /// @dev Max amount of tokens registered in the network (excluding ETH, which is hardcoded as tokenId = 0)
-    uint16 constant MAX_AMOUNT_OF_REGISTERED_TOKENS = $(MAX_AMOUNT_OF_REGISTERED_TOKENS);
+    uint16 constant MAX_AMOUNT_OF_REGISTERED_TOKENS = 10240;
 
     /// @dev Max account id that could be registered in the network
     uint32 constant MAX_ACCOUNT_ID = (2**24) - 1;
@@ -59,7 +59,7 @@ contract Config {
 
     /// @dev Expiration delta for priority request to be satisfied (in ETH blocks)
     uint256 constant PRIORITY_EXPIRATION =
-        $(defined(PRIORITY_EXPIRATION) ? PRIORITY_EXPIRATION : PRIORITY_EXPIRATION_PERIOD / BLOCK_PERIOD);
+           PRIORITY_EXPIRATION_PERIOD / BLOCK_PERIOD;
 
     /// @dev Maximum number of priority request to clear during verifying the block
     /// @dev Cause deleting storage slots cost 5k gas per each slot it's unprofitable to clear too many slots
@@ -75,11 +75,8 @@ contract Config {
     /// @dev Notice period before activation preparation status of upgrade mode (in seconds)
     /// @dev NOTE: we must reserve for users enough time to send full exit operation, wait maximum time for processing this operation and withdraw funds from it.
     uint256 constant UPGRADE_NOTICE_PERIOD =
-        $(
-            defined(UPGRADE_NOTICE_PERIOD)
-                ? UPGRADE_NOTICE_PERIOD
-                : MASS_FULL_EXIT_PERIOD + PRIORITY_EXPIRATION_PERIOD + TIME_TO_WITHDRAW_FUNDS_FROM_FULL_EXIT
-        );
+        MASS_FULL_EXIT_PERIOD + PRIORITY_EXPIRATION_PERIOD + TIME_TO_WITHDRAW_FUNDS_FROM_FULL_EXIT;
+
 
     /// @dev Timestamp - seconds since unix epoch
     uint256 constant COMMIT_TIMESTAMP_NOT_OLDER = 24 hours;
@@ -89,7 +86,7 @@ contract Config {
     uint256 constant COMMIT_TIMESTAMP_APPROXIMATION_DELTA = 15 minutes;
 
     /// @dev Bit mask to apply for verifier public input before verifying.
-    uint256 constant INPUT_MASK = $$(~uint256(0) >> 3);
+    uint256 constant INPUT_MASK = (~uint256(0) >> 3);
 
     /// @dev Auth fact reset timelock
     uint256 constant AUTH_FACT_RESET_TIMELOCK = 1 days;
---
+++ contracts/truffle-config.js
@@ -0,0 +1,57 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const PrivateKeyProvider = require('truffle-privatekey-provider');
+const fs = require('fs');
+let secrets;
+if (fs.existsSync('secrets.json')) {
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+}
+
+module.exports = {
+  compilers: {
+    solc: {
+      version: "^0.7.0", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+      parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+    }
+  },
+  // Uncommenting the defaults below 
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  networks: {
+    rinkeby: {
+      host: "172.18.11.37",
+      port: 8545,
+      network_id: "4"
+    },
+    ropsten: {
+      host: "172.18.11.37",
+      port: 8545,
+      network_id: "3"
+    },
+    goerli: {
+      provider: new HDWalletProvider("forest antenna burden wheel prefer isolate claw fire chief iron happy nest", 'https://goerli.infura.io/v3/3be7a6998574443381559f7075192e70'),
+      host: "172.18.11.36",
+      port: 10234,
+      network_id: "5",
+      networkCheckTimeout: 999999, 
+    },
+    heco: {
+	
+      network_id: "256",
+    },
+    local: {
+      host: "172.18.18.141",
+      port: 8545,
+      network_id: "*"
+    },
+    test: {
+      provider: new PrivateKeyProvider("0dae11faa7b5075c426a88888f4d2250aeea58b2f0c68c4c428f28df8d56e129", 'https://http-testnet.huobichain.com'),	
+      network_id: "256",
+      gas: 0x600000, 
+    }
+  }
+  //
+};
